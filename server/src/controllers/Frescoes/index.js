4721dc8bc74c20e877bab831ac5aa62beff1a20f
appshore <bmichel@appshore.co.uk>
2021-07-09 08:47:42 +0200

    ckeanup unwanted code

--- server/src/controllers/Frescoes/index.js
+++
@@ -1,6 +0,0 @@
-import deleteFresco from './delete';
-import getFresco from './get';
-import listFrescoes from './list';
-import listByYearFrescoes from './listByYear';
-
-export { deleteFresco, getFresco, listFrescoes, listByYearFrescoes };
